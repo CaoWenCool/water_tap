cf4571e3ace3a9b16005eaab2af8121352b9f976
CaoWenCool <1042879511@qq.com>
2022-08-13 08:57:43 +0800

    项目初始化

---
+++ watertap/src/main/java/com/blockchain/watertap/WatertapApplication.java
@@ -0,0 +1,13 @@
+package com.blockchain.watertap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatertapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatertapApplication.class, args);
+    }
+
+}
