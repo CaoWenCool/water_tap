f46e4d0c69066ab2cf932b71f2abbc63269b71e0
CaoWenCool <1042879511@qq.com>
2022-08-14 09:34:17 +0800

    启动脚本

---
+++ watertap/deploy/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ps ux | grep "watertap" | grep -v grep | grep -v stop.sh | cut -c 9-15 | xargs kill -s 9
+
+for var in $@
+do
+    ps ux | grep "$var" | grep -v grep | grep -v stop.sh | cut -c 9-15 | xargs kill -s 9
+done
+
+echo "kill $@ done"
