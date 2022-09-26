86bc6d43bada0ad9a64f87a42886cb131e2fa26f
CaoWenCool <1042879511@qq.com>
2022-09-27 06:24:34 +0800

    数据货币的配置

--- watertap/src/main/java/com/blockchain/watertap/util/Testutil.java
+++ watertap/src/main/java/com/blockchain/watertap/util/Testutil.java
@@ -6,10 +6,291 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 public class Testutil {
 
     public static void main(String args[]) throws JsonProcessingException {
-        String str = "{code=1.0, msg=成功, data=0.452878638825502756}";
-        str = str.replace("{","");
-        str = str.replace("}","");
-        String[] strList = str.split("=");
-        System.out.println(strList[3]);
+        String str = "[\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"initialSupply\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"nonpayable\",\n" +
+                "\t\t\"type\": \"constructor\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"anonymous\": false,\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"indexed\": true,\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"owner\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"indexed\": true,\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"spender\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"indexed\": false,\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"value\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"name\": \"Approval\",\n" +
+                "\t\t\"type\": \"event\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"anonymous\": false,\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"indexed\": true,\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"from\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"indexed\": true,\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"to\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"indexed\": false,\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"value\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"name\": \"Transfer\",\n" +
+                "\t\t\"type\": \"event\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"owner\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"spender\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"name\": \"allowance\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"view\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"spender\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"amount\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"name\": \"approve\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"bool\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"bool\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"nonpayable\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"account\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"name\": \"balanceOf\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"view\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [],\n" +
+                "\t\t\"name\": \"decimals\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint8\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"uint8\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"view\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"spender\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"subtractedValue\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"name\": \"decreaseAllowance\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"bool\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"bool\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"nonpayable\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"spender\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"addedValue\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"name\": \"increaseAllowance\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"bool\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"bool\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"nonpayable\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [],\n" +
+                "\t\t\"name\": \"name\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"string\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"string\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"view\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [],\n" +
+                "\t\t\"name\": \"symbol\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"string\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"string\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"view\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [],\n" +
+                "\t\t\"name\": \"totalSupply\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"view\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"recipient\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"amount\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"name\": \"transfer\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"bool\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"bool\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"nonpayable\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"inputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"sender\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"address\",\n" +
+                "\t\t\t\t\"name\": \"recipient\",\n" +
+                "\t\t\t\t\"type\": \"address\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"uint256\",\n" +
+                "\t\t\t\t\"name\": \"amount\",\n" +
+                "\t\t\t\t\"type\": \"uint256\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"name\": \"transferFrom\",\n" +
+                "\t\t\"outputs\": [\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"internalType\": \"bool\",\n" +
+                "\t\t\t\t\"name\": \"\",\n" +
+                "\t\t\t\t\"type\": \"bool\"\n" +
+                "\t\t\t}\n" +
+                "\t\t],\n" +
+                "\t\t\"stateMutability\": \"nonpayable\",\n" +
+                "\t\t\"type\": \"function\"\n" +
+                "\t}\n" +
+                "]\n";
+
+        str = str.replace("\t","").replace("\n","");
+        System.out.println(str);
     }
 }
