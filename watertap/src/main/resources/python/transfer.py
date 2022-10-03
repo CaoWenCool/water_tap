87fe709d9bd49eff31739f2a2d6dca6965a600a1
CaoWenCool <1042879511@qq.com>
2022-10-03 11:40:44 +0800

    数据货币的配置

--- watertap/src/main/resources/python/transfer.py
+++ watertap/src/main/resources/python/transfer.py
@@ -1,11 +1,6 @@
-#!/usr/local/bin python3
+#!/usr/local/bin/python3
 
-import os
-import sys
 import json
-
-_TOP_DIR = os.path.abspath("/usr/local/python3/lib/python3.7/site-packages")
-sys.path.insert(0, _TOP_DIR)
 
 from web3 import Web3, HTTPProvider
 from eth_account import Account
@@ -21,5 +16,30 @@ def transfer(network, token_address, abi, private_key, to_address, trans_value):
     :param trans_value: 交易金额
     :return:
     """
-    print(to_address)
-    return token_address
+    w3 = Web3(HTTPProvider(network))  # 网络地址
+    token_contract = Web3.toChecksumAddress(token_address)  #合约地址
+    abi_json = json.loads(abi)
+    token_contract = w3.eth.contract(address=token_contract, abi=abi_json)
+    print(w3.isConnected())
+    acc = Account.from_key(private_key=private_key)
+    from_address = Web3.toChecksumAddress(acc.address)
+    balance = token_contract.functions.balanceOf(from_address).call()
+    print(Web3.toWei(balance, 'ether'))
+    # TODO 添加余额的判断
+    to_address = Web3.toChecksumAddress(to_address)
+    nonce = w3.eth.getTransactionCount(from_address)
+    gas_price = w3.eth.gasPrice
+    trans_value = float(trans_value)
+    value = Web3.toWei(trans_value, 'ether')
+    balance = Web3.toWei(balance, 'ether')
+    if balance < value:
+        return -1
+    gas = token_contract.functions.transfer(to_address, int(value)).estimateGas({'from': from_address})
+    transaction_contract = token_contract.functions.transfer(to_address, int(value)).buildTransaction(
+        {'gasPrice': gas_price, 'gas': gas, 'nonce': nonce})
+    txn_signed_usdt = w3.eth.account.signTransaction(transaction_contract,private_key)
+    txn_hash = w3.eth.sendRawTransaction(txn_signed_usdt.rawTransaction)
+    txn_hash = Web3.toHex(txn_hash)
+    return txn_hash
+
+
