6df26abe14d4fbd02086a85b8e4e7b04c99bfeba
CaoWenCool <1042879511@qq.com>
2022-10-03 16:22:06 +0800

    数据货币的配置

--- watertap/start.sh
+++ watertap/start.sh
@@ -4,4 +4,4 @@ mvn clean package  -Dmaven.test.skip=true
 cd /home/java/water_tap/watertap/deploy
 sh start.sh
 
-pyhton3 /home/java/water_tap/watertap/src/main/resources/python/transfer.py
+python3 /home/java/water_tap/watertap/src/main/resources/python/transfer.py
